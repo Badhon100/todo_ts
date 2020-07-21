db8a598232b9395044aa58fbf1b432fdbb707381
Badhon100 <badhonx102@gmail.com>
2020-07-21 22:30:37 +0600

    designed front end

---
+++ app.js
@@ -0,0 +1,7 @@
+"use strict";
+var button = document.querySelector("button");
+var input = document.getElementById("input");
+function list(result) {
+    result = input.value;
+    console.log(result);
+}
---
+++ app.ts
@@ -0,0 +1,8 @@
+const button = document.querySelector("button");
+let input = document.getElementById("input")! as HTMLInputElement;
+
+
+function list(result: string){
+    result = input.value;
+    console.log(result);
+}
